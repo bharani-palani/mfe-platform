1f4b789706479d55c89b399b6fdc244804f2c820
Bharani <tp.bharani@gmail.com>
2026-03-13 18:41:03 +0530

    federation + port init

--- apps/shell/vite.config.ts
+++ apps/shell/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    federation({
+      name: 'shellApp',
+      filename: 'remoteEntry.js',
+      exposes: {},
+      remotes: {},
+      shared: ['react', 'react-dom']
+    })
   ],
+   server: {
+    port: 4002,
+    strictPort: true,
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  }
 })
