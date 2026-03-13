405c1db201dbff2261d8bbce616f5ce38a5ae2ac
Bharani <tp.bharani@gmail.com>
2026-03-13 18:20:11 +0530

    cleanup

--- apps/ledgerely/src/App.tsx
+++ apps/ledgerely/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
       <section>
-        Shell App
+        Ledgerely App
       </section>
   )
 }
--- apps/officeAdmin/src/App.tsx
+++ apps/officeAdmin/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
       <section>
-        Shell App
+        Product App
       </section>
   )
 }
