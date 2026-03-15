bd0ee4cd6dc1d268460dae3181196b439f07b9e0
Bharani <tp.bharani@gmail.com>
2026-03-15 17:52:03 +0530

    deploy test

--- apps/ledgerely/src/App.tsx
+++ apps/ledgerely/src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import '@repo/tailwind-config/styles'
 
 function App() {
-  return <section>Ledgerely App</section>
+  return (
+    <section>
+      <div className="text-3xl bg-red-200 p-2">Ledgerely App</div>
+    </section>
+  )
 }
 
 export default App
